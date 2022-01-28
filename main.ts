0b7dfafd95eb4eb6ecfdb85d411b26c9caa9bab9
LeoLaurent <59579418+LeoLaurent@users.noreply.github.com>
2022-01-28 15:17:41 +0100

    Mise à jour pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,56 +1,63 @@
+namespace SpriteKind {
+    export const eolienne = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 100, 0)
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`myAnim0`,
+    100,
+    true
+    )
+    animation.runImageAnimation(
+    projectile,
+    assets.animation`myAnim`,
+    1000,
+    true
+    )
+    pause(250)
+    animation.stopAnimation(animation.AnimationTypes.All, mySprite)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
 class ActionKind {
     static Walking = 0
     static Idle = 1
     static Jumping = 2
 }
-
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
-    
-    projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . .
-        `, mySprite, 50, 0)
-    animation.runImageAnimation(mySprite, assets.animation`
-        myAnim0
-    `, 100, true)
-    animation.runImageAnimation(projectile, assets.animation`
-        myAnim
-    `, 1000, true)
-    pause(250)
-    animation.stopAnimation(animation.AnimationTypes.All, mySprite)
-})
-let projectile : Sprite = null
-let mySprite : Sprite = null
 let camx = 80
-let playerx = 20
-tiles.setTilemap(tilemap`
-    niveau1
-`)
+let playerx = 40
+let eoliennex = 20
+let eolienne = sprites.create(assets.image`myImage1`, SpriteKind.eolienne)
+eolienne.setPosition(eoliennex, 65)
+tiles.setTilemap(tilemap`niveau1`)
 scene.centerCameraAt(camx, 0)
-mySprite = sprites.create(assets.image`
-    myImage
-`, SpriteKind.Player)
-mySprite.setPosition(playerx, 50)
+mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+mySprite.setPosition(playerx, 110)
 controller.moveSprite(mySprite)
 mySprite.setStayInScreen(true)
-forever(function on_forever() {
-    
+forever(function () {
     playerx += 1
+    eoliennex += 1
     camx += 1
     mySprite.x = playerx
+    eolienne.x = eoliennex
     scene.centerCameraAt(camx, 0)
 })
