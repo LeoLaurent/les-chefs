6472c10e0f059974826a34b9537a4d4a05ce3a91
LeoLaurent <59579418+LeoLaurent@users.noreply.github.com>
2022-01-28 16:23:48 +0100

    Mise à jour main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -32,16 +32,33 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     1000,
     true
     )
+    tab_projectile.unshift(projectile)
     pause(250)
     animation.stopAnimation(animation.AnimationTypes.All, mySprite)
 })
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.startEffect(effects.ashes)
+    mySprite.sayText("+1")
+    sprite.destroy()
+    otherSprite.destroy()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.startEffect(effects.ashes)
+    otherSprite.destroy()
+    sprite.sayText("OOF")
+})
+let piece: Sprite = null
+let diceroll = 0
 let projectile: Sprite = null
 let mySprite: Sprite = null
+let tab_projectile: Sprite[] = []
 class ActionKind {
     static Walking = 0
     static Idle = 1
     static Jumping = 2
 }
+tab_projectile = []
+let tab_piece: Sprite[] = []
 let camx = 80
 let playerx = 40
 let eoliennex = 20
@@ -60,4 +77,91 @@ forever(function () {
     mySprite.x = playerx
     eolienne.x = eoliennex
     scene.centerCameraAt(camx, 0)
+    diceroll = randint(0, 150)
+    if (diceroll == 0) {
+        piece = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        piece.setPosition(eoliennex + 150, randint(10, 100))
+        animation.runImageAnimation(
+        piece,
+        assets.animation`myAnim2`,
+        300,
+        true
+        )
+        tab_piece.unshift(piece)
+    } else {
+        if (diceroll == 1) {
+            piece = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.Enemy)
+            piece.setPosition(eoliennex + 150, randint(10, 100))
+            animation.runImageAnimation(
+            piece,
+            assets.animation`myAnim1`,
+            200,
+            true
+            )
+            tab_piece.unshift(piece)
+        } else {
+            if (diceroll == 2) {
+                piece = sprites.create(img`
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    `, SpriteKind.Enemy)
+                piece.setPosition(eoliennex + 150, randint(10, 100))
+                animation.runImageAnimation(
+                piece,
+                assets.animation`myAnim3`,
+                200,
+                true
+                )
+                tab_piece.unshift(piece)
+            }
+        }
+    }
 })
